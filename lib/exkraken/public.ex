56c35325ea1d6c183c38777be619ed941e3df6a0
Christoph Geschwind <christophgeschwind@googlemail.com>
2017-08-13 14:41:44 +0200

    Add ExDoc

--- lib/exkraken/public.ex
+++ lib/exkraken/public.ex
@@ -3,26 +3,24 @@ defmodule Exkraken.Public do
   Public API methods
   """
 
-  use HTTPoison.Base
-
-  def process_url(url) do
+  defp process_url(url) do
     Application.get_env(:exkraken, :endpoint) <> url
   end
 
-  def process_response_body(body) do
+  defp process_response_body(body) do
     body
     |> Poison.decode!
   end
 
-  def get_result(url) do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = get(url)
+  defp get_result(url) do
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = HTTPoison.Base.get(url)
     {:ok, Map.get(body, "result")}
   end
 
   @doc """
   Get server time
+
   URL: https://api.kraken.com/0/public/Time
-  Result: Server's time
 
   ## Examples
 
@@ -40,6 +38,7 @@ defmodule Exkraken.Public do
 
   @doc """
   Get asset info
+
   URL: https://api.kraken.com/0/public/Assets
 
   ## Examples
@@ -47,27 +46,39 @@ defmodule Exkraken.Public do
       iex> Exkraken.Public.get_asset_info(%{asset: ["XXBT", "ZEUR"]})
       {:ok, %{"XXBT" => %{...}, ...}}
   """
-  def get_asset_info do
-    get_result("/0/public/Assets")
-  end
   def get_asset_info(%{asset: asset_list}) do
     get_result("/0/public/Assets?" <> URI.encode_query(asset: Enum.join(asset_list, ",")))
+  end
+  def get_asset_info do
+    get_result("/0/public/Assets")
   end
 
   @doc """
   Get tradable asset pairs
+
   URL: https://api.kraken.com/0/public/AssetPairs
+
+  ## Examples
+
+      iex> Exkraken.Public.get_tradable_asset_pairs(%{pair: ["XXBTZEUR"]})
+      {:ok, %{"XXBTZEUR" => %{...}}}
   """
-  def get_tradable_asset_pairs do
-    get_result("/0/public/AssetPairs")
-  end
   def get_tradable_asset_pairs(%{pair: pair_list}) do
     get_result("/0/public/AssetPairs?" <> URI.encode_query(pair: Enum.join(pair_list, ",")))
+  end
+  def get_tradable_asset_pairs do
+    get_result("/0/public/AssetPairs")
   end
 
   @doc """
   Get ticker information
+
   URL: https://api.kraken.com/0/public/Ticker
+
+  ## Examples
+
+      iex> Exkraken.Public.get_ticker_information(%{pair: ["XXBTZEUR"]})
+      {:ok, %{"XXBTZEUR" => %{...}}}
   """
   def get_ticker_information(%{pair: pair_list}) do
     get_result("/0/public/Ticker?" <> URI.encode_query(pair: Enum.join(pair_list, ",")))
--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule Exkraken.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: 'Exkraken',
+      source_url: "https://github.com/1st8/exkraken",
+      docs: [main: "Exkraken", # The main page in the docs
+       # logo: "path/to/logo.png",
+       extras: ["README.md"]]
     ]
   end
 
@@ -28,7 +35,8 @@ defmodule Exkraken.Mixfile do
       {:httpoison, "~> 0.12"},
       {:poison, "~> 3.1"},
       {:exvcr, "~> 0.8", only: :test},
-      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
 end
